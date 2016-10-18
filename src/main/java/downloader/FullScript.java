d5c16c7ee021dfa936f73a04555f679c8489391c
bipin <bipinbipin@gmail.com>
2016-10-18 00:15:57 -0500

    Got video to download

--- src/main/java/downloader/FullScript.java
+++ src/main/java/downloader/FullScript.java
@@ -1,9 +1,13 @@
 package downloader;
 
+import downloader.domain.VideoInfo;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * Created by bipin on 10/17/16.
@@ -15,22 +19,64 @@ public class FullScript {
         FullScript obj = new FullScript();
 
         String link = "https://www.youtube.com/watch?v=mBmL7iZpCb0";
-        String output = obj.get_youtube_video_info(link);
-        System.out.println(output);
+        String info = obj.get_youtube_video_info(link);
+        VideoInfo videoInfo = obj.parse_video_info(info);
 
-        JSONObject jsonObject = new JSONObject(output);
+        //obj.download_video(videoInfo.getUrl());
+        System.out.println(videoInfo.toString());
 
-        String ID = jsonObject.getString("id");
-        String PAGE_TITLE = jsonObject.getString("page_title");
-        String URL = jsonObject.getJSONArray("link").getJSONObject(0).getString("url");
-        System.out.println(ID);
-        System.out.println(PAGE_TITLE);
-        System.out.println(URL);
+
+        obj.ffmpeg_download_video(videoInfo.getUrl());
 
 
     }
 
 
+    private void ffmpeg_download_video(String url) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder("ffmpeg", "-i", url, "-vcodec", "copy", "-acodec", "copy", "out.mkv");
+            final Process p = pb.start();
+
+            new Thread() {
+                public void run() {
+
+                    Scanner sc = new Scanner(p.getErrorStream());
+
+                    // Find duration
+                    Pattern durPattern = Pattern.compile("(?<=Duration: )[^,]*");
+                    String dur = sc.findWithinHorizon(durPattern, 0);
+                    if (dur == null)
+                        throw new RuntimeException("Could not parse duration.");
+                    String[] hms = dur.split(":");
+                    double totalSecs = Integer.parseInt(hms[0]) * 3600
+                            + Integer.parseInt(hms[1]) * 60
+                            + Double.parseDouble(hms[2]);
+                    System.out.println("Total duration: " + totalSecs + " seconds.");
+
+                    // Find time as long as possible.
+                    Pattern timePattern = Pattern.compile("(?<=time=)[\\d.]*");
+                    String match;
+                    while (null != (match = sc.findWithinHorizon(timePattern, 0))) {
+                        double progress = Double.parseDouble(match) / totalSecs;
+                        System.out.printf("Progress: %.2f%%%n", progress * 100);
+                    }
+                }
+            }.start();
+        } catch (IOException ioEx) {
+            System.out.println(ioEx);
+        }
+    }
+
+    private VideoInfo parse_video_info(String info) {
+
+        JSONObject jsonObject = new JSONObject(info);
+        VideoInfo videoInfo = new VideoInfo();
+        videoInfo.setId(jsonObject.getString("id"));
+        videoInfo.setTitle(jsonObject.getString("page_title"));
+        videoInfo.setUrl(jsonObject.getJSONArray("link").getJSONObject(0).getString("url"));
+
+        return videoInfo;
+    }
 
     private String get_youtube_video_info(String link) {
 
---
+++ src/main/java/downloader/domain/VideoInfo.java
@@ -0,0 +1,53 @@
+package downloader.domain;
+
+/**
+ * Created by bipin on 10/17/16.
+ */
+public class VideoInfo {
+
+    String id;
+    String title;
+    String url;
+
+    public VideoInfo() {
+    }
+
+    public VideoInfo(String id, String title, String url) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "VideoInfo{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
