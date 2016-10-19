119396864e30e37f3ffe95134bf12d42f0813de7
bipin <bipinbipin@gmail.com>
2016-10-18 22:17:09 -0500

    got thru all processing.
    Remaining:
       - edge cases for some vids that don't download
       - adding the image to the mp3 fucks it up
       - batch/mass processing

--- src/main/java/downloader/FullScript.java
+++ src/main/java/downloader/FullScript.java
@@ -115,21 +115,21 @@ public class FullScript {
                     // Find duration
                     Pattern durPattern = Pattern.compile("(?<=Duration: )[^,]*");
                     String dur = sc.findWithinHorizon(durPattern, 0);
-                    //if (dur == null)
-                        //throw new RuntimeException("Could not parse duration.");
-                    //String[] hms = dur.split(":");
-                    //double totalSecs = Integer.parseInt(hms[0]) * 3600
-                    //        + Integer.parseInt(hms[1]) * 60
-                    //        + Double.parseDouble(hms[2]);
-                    //System.out.println("Total duration: " + totalSecs + " seconds.");
+                    if (dur == null)
+                        throw new RuntimeException("Could not parse duration.");
+                    String[] hms = dur.split(":");
+                    double totalSecs = Integer.parseInt(hms[0]) * 3600
+                            + Integer.parseInt(hms[1]) * 60
+                            + Double.parseDouble(hms[2]);
+                    System.out.println("Total duration: " + totalSecs + " seconds.");
 
                     // Find time as long as possible.
                     Pattern timePattern = Pattern.compile("(?<=time=)[\\d.]*");
                     String match;
-                    //while (null != (match = sc.findWithinHorizon(timePattern, 0))) {
-                    //    double progress = Double.parseDouble(match) / totalSecs;
-                    //    System.out.printf("Progress: %.2f%%%n", progress * 100);
-                    //}
+                    while (null != (match = sc.findWithinHorizon(timePattern, 0))) {
+                        double progress = Double.parseDouble(match) / totalSecs;
+                        System.out.printf("Progress: %.2f%%%n", progress * 100);
+                    }
                 }
             }.start();
         } catch (IOException ioEx) {
